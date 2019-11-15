9c91adcd7cc01eeeb96209d5591c5551a92c00bf
Camila Santa Maria Luna <camilorris97@gmail.com>
2019-11-14 22:26:40 -0500

    Fix Font Styles

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,7 +1,34 @@
 import React from "react";
 import ReactSelect from "react-select";
 
-import customStyles from "./customStyles";
+const customStyles = {
+  option: (provided, state) => ({
+    ...provided,
+    border: "none",
+    padding: 10,
+    fontFamily: "Arial",
+    fontSize: "12px",
+    cursor: "pointer"
+  }),
+  singleValue: (provided, state) => ({
+    ...provided,
+    fontFamily: "Bungee-Regular",
+    color: "#6a3091",
+    fontSize: "14px"
+  }),
+  container: (provided, state) => ({
+    ...provided,
+    alignItems: "center",
+    width: "30%",
+    margin: "28px auto"
+  }),
+  control: (provided, state) => ({
+    ...provided,
+    cursor: "pointer",
+    border: "none",
+    boxShadow: "0px 1px 2px 0px rgba(0, 0, 0, 0.2)}"
+  })
+};
 
 const Select = ({ options, onChange, defaultValue }) => (
   <ReactSelect
